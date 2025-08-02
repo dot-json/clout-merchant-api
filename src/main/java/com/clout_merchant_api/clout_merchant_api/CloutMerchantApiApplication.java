385067afd32e9eb38101ac310543b0b7536d2414
dot-json <zsombor.gazdag@gmail.com>
2025-08-02 23:24:23 +0200

    init

---
+++ src/main/java/com/clout_merchant_api/clout_merchant_api/CloutMerchantApiApplication.java
@@ -0,0 +1,13 @@
+package com.clout_merchant_api.clout_merchant_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloutMerchantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloutMerchantApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clout_merchant_api/clout_merchant_api/CloutMerchantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clout_merchant_api.clout_merchant_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloutMerchantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
